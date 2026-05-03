c016c40685dde66cf3bf4178dcd09ad79f66d03b
ALEC-xy <140408673+ALEC-xy@users.noreply.github.com>
2026-05-03 07:32:05 +0100

    Fix syntax error in bundle_checker_bot.py

--- bundle_checker_bot.py
+++ bundle_checker_bot.py
@@ -46,7 +46,7 @@ EXCLUDE_ADDRS = {
     "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1",
     "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8",
     "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P",
-} it
+}
 
 PUMP_MINT_AUTH = "TSLvdd1pWpHVjahSpsvCXUbgwsL3JAcvokwaKt1eokM"
 
